636c2519fd108effd406ae33142086a919fe7885
Diego Isaac Herrera Ledezma <dhlisaac@gmail.com>
2022-01-12 12:55:38 -0600

    publica en facebook e instagram

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
+const { ProductCatalogProductSetsBatch } = require('facebook-nodejs-business-sdk')
+
+require('dotenv').config()
+
 module.exports = {
   reactStrictMode: true,
+  env:{
+    FACEBOOK_APP_ID : process.env.FACEBOOK_APP_ID
+  },
 }
